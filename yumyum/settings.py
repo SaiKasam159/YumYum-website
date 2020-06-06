b059ef1e1f4b71ad8063158a719e83ec880399cb
Sai Kasam <sai.k.kasam@gmail.com>
2020-06-06 10:11:27 +0000

    removed old stuff

--- yumyum/settings.py
+++ yumyum/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g=+g%-f)lzib5yupe09xw6qx#tfpo9z9oujd6r#8c!#g6!dv!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bojanum.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 LOGIN_URL = '/login/'
